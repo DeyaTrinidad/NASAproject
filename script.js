9e66605cffcfe039b18b7122f96a401cd79677b6
Carlos Zamora <czamora5@ucol.mx>
2022-08-30 21:48:29 -0500

    Add api functions

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import {getPictureOfTheDay, getPicture, getData, getPicturesRange, getRandomPictures} from './api.js';
+
+const BASE_URL = 'https://api.nasa.gov/planetary/apod';
+const API_KEY = 'api_key=t6DwZgp9FbxOvDNJeqXlI8zvZmDudZRyP5vSH4Nu';
 /*
 const Search = {
   template: `<div class="search">
@@ -76,49 +80,91 @@ new Vue({
 
 //Script del proyecto pasado Nancy
 //Método Sensei
-fetch('https://images-api.nasa.gov/search?q=${busqueda}')
-.then((response)=>response.json())
-.then((data)=>{
-    resultados = (data);
-    dibujar(data)
-})
+// fetch('https://images-api.nasa.gov/search?q=${busqueda}')
+// .then((response)=>response.json())
+// .then((data)=>{
+//     const resultados = (data);
+//     dibujar(data)
+// })
 
 
-const buscar = (evt)=>{
-    console.log(evt.currentTarget.value);
+// const buscar = (evt)=>{
+//     console.log(evt.currentTarget.value);
 
-    resultados.filter(function(resultado){
-        return 
-    })
+//     resultados.filter(function(resultado){
+//         return 
+//     })
+// }
+
+// const dibujar = (fichas) => {
+    
+//     document.querySelector("#galeria").innerHTML="";
+//     fichas.forEach((ficha) => {
+//         let div = document.createElement("div");
+//         div.classList.add("column", "is-3");
+//         div.innerHTML += `<div class="card">
+//                         <div class="card-image">
+//                             <figure class="image is-4by3">
+//                                 <img src="${personaje.href}" alt="Placeholder image">
+//                             </figure>
+//                         </div>
+//                         <div class="card-content">
+//                             <div class="media">
+//                                 <div class="media-content">
+//                                 <p class="title is-4"><b>${personaje.title}</b></p>
+//                                 </div>
+//                             </div>
+//                         <div class="content">
+//                             <p><b>${personaje.description}<b/></p>
+//                             <a>@Nasa</a>
+//                             <br>
+//                             <time datetime="${personaje.date_created}"></time>
+//                         </div>
+//                         </div>
+//                     </div> `
+//         document.querySelector("#galeria").append(div);
+//     });
+
+// }
+
+
+const run = async () => {
+  const data = await fetch(`${BASE_URL}?${API_KEY}&start_date=2020-03-10&end_date=2022-08-30`);
+  const response = await data.json();
+  console.log(response);
+  displayPictures(response);
 }
 
-const dibujar = (fichas) => {
-    
-    document.querySelector("#galeria").innerHTML="";
-    fichas.forEach((ficha) => {
-        let div = document.createElement("div");
-        div.classList.add("column", "is-3");
-        div.innerHTML += `<div class="card">
-                        <div class="card-image">
-                            <figure class="image is-4by3">
-                                <img src="${personaje.href}" alt="Placeholder image">
-                            </figure>
-                        </div>
-                        <div class="card-content">
-                            <div class="media">
-                                <div class="media-content">
-                                <p class="title is-4"><b>${personaje.title}</b></p>
-                                </div>
-                            </div>
-                        <div class="content">
-                            <p><b>${personaje.description}<b/></p>
-                            <a>@Nasa</a>
-                            <br>
-                            <time datetime="${personaje.date_created}"></time>
-                        </div>
-                        </div>
-                    </div> `
-        document.querySelector("#galeria").append(div);
-    });
+fetch(`${BASE_URL}?${API_KEY}`).then(data => data.json()).then(data => displayPictures(data))
 
-}  
+run();
+
+function displayPictures(data) {
+  console.log(data)
+  const fragment = document.createDocumentFragment();
+  data.forEach(pictureData => {
+    console.log(pictureData)
+    if (pictureData['media_type'] !== 'image') return;
+
+    const img = document.createElement('img');
+    img.src = pictureData.url;
+
+    const h3 = document.createElement('h3');
+    h3.textContent = pictureData.title;
+
+    const p = document.createElement('p');
+    p.textContent = pictureData.date;
+
+    const div = document.createElement('div');
+    div.append(img, h3, p);
+
+    fragment.appendChild(div);
+  });
+  document.getElementById('galeria').appendChild(fragment);
+}
+
+// console.log(getPictureOfTheDay());
+
+fetch('https://api.nasa.gov/planetary/apod?api_key=t6DwZgp9FbxOvDNJeqXlI8zvZmDudZRyP5vSH4Nu')
+  .then(res => res.json())
+  .then(data => console.log(data));
